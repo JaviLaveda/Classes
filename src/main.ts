8e877fa9676bf8251340e2ce374056d63aa07568
JaviLaveda <javi.laveda.dev@gmail.com>
2024-02-16 10:34:54 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,134 +1,81 @@
-import {
-  obtenerPersonajes,
-  // obtenerPersonajesFiltrados,
-} from "./personajes-listado.api";
-import {
-  urlHost,
-  urlHostPersonajes,
-  urlHostSearch,
-  Personaje,
-} from "./personajes-listado.model";
+const myregex = /<img[^>]+src=["']([^"']+)["']/g;
 
-const crearElementoImagen = (nombre: string): HTMLImageElement => {
+const obtenerEnlaces = (texto: string): string[] => {
+  const enlaces: string[] = [];
+  let match;
+  while ((match = myregex.exec(texto)) !== null) {
+    enlaces.push(match[1]);
+  }
+  return enlaces;
+};
+
+const crearElementoParrafo = (texto: string): HTMLParagraphElement => {
+  const parrafo = document.createElement("p");
+  parrafo.textContent = texto;
+  return parrafo;
+};
+
+const crearElementoImagen = (texto: string): HTMLImageElement => {
   const imagen = document.createElement("img");
-  imagen.src = `${urlHost}/${nombre}`;
+  imagen.src = texto;
+  imagen.classList.add("imagen-card");
   return imagen;
 };
 
-const crearElementoParrafo = (
-  campo: string,
-  texto: string
-): HTMLParagraphElement => {
-  const parrafo = document.createElement("p");
-  parrafo.textContent = campo + texto;
-  return parrafo;
-};
-const crearContenedorPersonaje = (personaje: Personaje): HTMLDivElement => {
-  const elementoPersonaje = document.createElement("div");
-  elementoPersonaje.classList.add("personaje-contenedor");
+const mostrarEnlacesIMG = (enlaces: string[]) => {
+  const listadoLink = document.getElementById("listadoEnlaces");
+  if (listadoLink && listadoLink instanceof HTMLDivElement) {
+    listadoLink.innerHTML = "";
 
-  const imagen = crearElementoImagen(personaje.imagen);
-  elementoPersonaje.appendChild(imagen);
-
-  const nombre = crearElementoParrafo("Nombre: ", personaje.nombre);
-  elementoPersonaje.appendChild(nombre);
-
-  const apodo = crearElementoParrafo("Apodo: ", personaje.apodo);
-  elementoPersonaje.appendChild(apodo);
-
-  const especialidad = crearElementoParrafo(
-    "Especialidad: ",
-    personaje.especialidad
-  );
-  elementoPersonaje.appendChild(especialidad);
-
-  const habilidades = crearElementoParrafo(
-    "Habilidades: ",
-    personaje.habilidades
-  );
-  elementoPersonaje.appendChild(habilidades);
-
-  const amigo = crearElementoParrafo("Amigo: ", personaje.amigo);
-  elementoPersonaje.appendChild(amigo);
-
-  return elementoPersonaje;
-};
-
-const crearContenedoresPersonajes = (
-  listado: HTMLDivElement,
-  personajes: Personaje[]
-) => {
-  personajes.forEach((personaje) => {
-    const contenedorPersonaje = crearContenedorPersonaje(personaje);
-    listado.appendChild(contenedorPersonaje);
-  });
-};
-
-const obtenerTerminoBusqueda = (): string => {
-  const searchInput = document.getElementById("busquedaInput");
-  if (searchInput && searchInput instanceof HTMLInputElement) {
-    return searchInput.value.toLowerCase().trim();
+    if (enlaces.length === 0) {
+      listadoLink.innerText = "No se encontraron enlaces";
+    } else {
+      enlaces.forEach((enlace) => {
+        const link = crearElementoParrafo(enlace);
+        listadoLink.appendChild(link);
+      });
+    }
   } else {
-    throw new Error("No se ha encontrado el input");
+    throw new Error("DivElement not found");
   }
 };
 
-const mensajeErrorListadoPersonajes = (listado: HTMLDivElement) => {
-  listado.innerHTML = "<p>No se encontraron personajes.</p>";
-};
+const mostrarIMGEncontradas = (enlaces: string[]) => {
+  const listadoLink = document.getElementById("listadoEnlaces");
+  if (listadoLink && listadoLink instanceof HTMLDivElement) {
+    listadoLink.innerHTML = "";
 
-const vaciarListadoPersonajes = (listado: HTMLDivElement) => {
-  listado.innerHTML = "";
-};
-
-const creaListadoPersonajes = (personajes: Personaje[]) => {
-  const listado = document.querySelector("#listado-personajes");
-  if (listado && listado instanceof HTMLDivElement) {
-    crearContenedoresPersonajes(listado, personajes);
+    if (enlaces.length === 0) {
+      listadoLink.innerText = "No se encontraron enlaces";
+    } else {
+      enlaces.forEach((enlace) => {
+        const img = crearElementoImagen(enlace);
+        listadoLink.appendChild(img);
+      });
+    }
   } else {
-    throw new Error("No se ha encontrado el contenedor del listado");
+    throw new Error("DivElement not found");
   }
 };
-
-const creaListadoPersonajesFiltrados = (personajes: Personaje[]) => {
-  const listado = document.querySelector("#listado-personajes");
-  if (personajes.length === 0 && listado && listado instanceof HTMLDivElement) {
-    mensajeErrorListadoPersonajes(listado);
-  } else if (listado && listado instanceof HTMLDivElement) {
-    vaciarListadoPersonajes(listado);
-    crearContenedoresPersonajes(listado, personajes);
+const extraerEnlacesIMG = () => {
+  const textArea = document.getElementById("textArea");
+  if (textArea && textArea instanceof HTMLTextAreaElement) {
+    const textoCopiado: string = textArea.value;
+    const enlacesEncontrados = obtenerEnlaces(textoCopiado);
+    // mostrarEnlacesIMG(enlacesEncontrados);
+    mostrarIMGEncontradas(enlacesEncontrados);
   } else {
-    throw new Error("No se ha encontrado el contenedor del listado");
+    throw new Error("TextArea not found");
   }
-};
-
-const mostrarPersonajes = async (): Promise<void> => {
-  const personajes = await obtenerPersonajes(urlHostPersonajes);
-
-  creaListadoPersonajes(personajes);
-};
-
-const mostrarPersonajesFiltrados = async (event: Event): Promise<void> => {
-  event.preventDefault();
-
-  const terminoBusqueda = obtenerTerminoBusqueda();
-  const personajesFiltrados = await obtenerPersonajes(
-    `${urlHostSearch}${terminoBusqueda}`
-  );
-
-  creaListadoPersonajesFiltrados(personajesFiltrados);
 };
 
 const events = () => {
-  const formulario = document.querySelector("#formulario");
-  if (formulario && formulario instanceof HTMLFormElement) {
-    formulario.addEventListener("submit", mostrarPersonajesFiltrados);
+  const btnExtraer = document.getElementById("btnExtraer");
+  if (btnExtraer && btnExtraer instanceof HTMLButtonElement) {
+    btnExtraer.addEventListener("click", extraerEnlacesIMG);
   } else {
-    throw new Error("FormElement not found");
+    throw new Error("BtnElement not found");
   }
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  mostrarPersonajes();
-  events();
-});
+document.addEventListener("DOMContentLoaded", events);
